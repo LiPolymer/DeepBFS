ee66ba47640ade4aa578ac6c7d4b4f1afba4d29e
LiPolymer <110595296+LiPolymer@users.noreply.github.com>
2024-09-26 23:20:13 +0800

    Initial commit

---
+++ DeepBFS/Program.cs
@@ -0,0 +1,8 @@
+﻿namespace DeepBFS;
+
+static class Program {
+    static void Main(string[] args) {
+        Console.WriteLine("=====DeepBFS=====");
+        Debug.WriteLine($"Startup Arguments[{args.Length}]:{Utils.GetStringArray(args)}");
+    }
+}
---
+++ DeepBFS/Util.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.RegularExpressions;
+
+namespace DeepBFS;
+
+public static class Utils{
+    public static string[] ResolveArgs(string st) {
+        return Regex.Matches(st, @"""([^""]*)""|(\S+)")
+            .Select(i => i.Value)
+            .ToArray();
+    }
+
+    public static string GetStringArray(string[] args) {
+        string buffer = string.Empty;
+        foreach (var child in args)
+        {
+            buffer += child;
+        }
+        return buffer;
+    }
+}
+
+public static class Debug {
+    public static void WriteLine(string msg) {
+        #if DEBUG
+            Console.WriteLine(msg);
+        #endif
+    }
+}
